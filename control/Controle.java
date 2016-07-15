58aec024a88566c951297a0fad28bafe52e1dbc6
aguiarroney <aguiarroney@gmail.com>
2016-07-14 21:20:14 -0300

    limpei alguns comentarios

--- control/Controle.java
+++ control/Controle.java
@@ -1,8 +1,4 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+
 package control;
 
 import java.text.SimpleDateFormat;
@@ -13,10 +9,7 @@ import model.Filial;
 import model.Veiculo;
 import view.Menu;
 
-/**
- *
- * @author Leo
- */
+
 public class Controle {
 
     private ArrayList<Filial> filiais;
